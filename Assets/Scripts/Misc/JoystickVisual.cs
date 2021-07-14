7c916d99f67493c9ce77c3b0f5f92e01f79d5e68
Grego <gregogamesYTcontact@gmail.com>
2021-07-13 23:54:07 -0400

    Final commit

---
+++ Assets/Scripts/Misc/JoystickVisual.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickVisual : MonoBehaviour
+{
+    private Vector3 initEuler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initEuler = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = initEuler + new Vector3(30.0f * -Input.GetAxis("Vertical"), 30.0f * -Input.GetAxis("Horizontal"));
+    }
+}
